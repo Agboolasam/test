b61237d00f4fa1fd240c77d428615c85c0c1b6b8
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-09-02 22:58:24 +0100

    some more edits on the css and js

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ let inputTodo = document.getElementById('input-todos');
 let addTodo = document.getElementById('add-todos');
 let itemList = document.getElementById('list-items');
 
+if (itemList.length = 0) {
+    let errMsg2 = document.getElementById('err-msg2');
+    errMsg2.style.display = "block"
+
+
+}
+
 
 function addItem() {
-    let errMsg = document.getElementById('err-msg');
+    let errMsg = document.getElementById('err-msg1');
     if (inputTodo.value === '') {
         errMsg.style.display = "block";
     } else {
@@ -15,9 +22,15 @@ function addItem() {
         let delBtn = document.createElement('button');
         delBtn.innerHTML = 'delete';
         li.appendChild(delBtn);
+        delBtn.id = "del-btn";
 
-        errMsg.style.display = "none";
+        let doneBtn = document.createElement('button');
+        doneBtn.innerHTML = 'Completed';
+        li.appendChild(doneBtn);
+        doneBtn.id = "complete-btn";
+
 
         inputTodo.value = '';
+        errMsg.style.display = "none";
     }
 }
