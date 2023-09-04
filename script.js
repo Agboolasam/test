f28df7caa2527f097fa9f3410d1c04dc40b1e67e
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-09-04 06:29:35 +0100

    resolved the functionality issues with the buttons

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let inputTodo = document.getElementById('input-todos');
 let addTodo = document.getElementById('add-todos');
 let itemList = document.getElementById('list-items');
+let errMsg1 = document.getElementById('err-msg1');
+let errMsg2 = document.getElementById('err-msg2');
 
-
+// add eventlistener to input box on presssing enter key
 inputTodo.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         addTodo.click();
@@ -10,16 +12,16 @@ inputTodo.addEventListener("keypress", (e) => {
 });
 
 
-
+// to add item to list
 function addItem() {
-    let errMsg1 = document.getElementById('err-msg1');
-    let errMsg2 = document.getElementById('err-msg2');
+
     if (inputTodo.value === '') {
         errMsg1.style.display = "block";
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputTodo.value;
         itemList.appendChild(li);
+        li.id = "new-item";
 
         let delBtn = document.createElement('button');
         delBtn.innerHTML = 'delete';
@@ -33,16 +35,45 @@ function addItem() {
 
         errMsg1.style.display = "none";
 
-        li.addEventListener("click", (e) => {
-            if (e.target.tagName == "button") {
-                document.confirm('ARE YOU SURE YOU WANT TO DELETE THIS TASK?');
-                e.parentElement.remove();
-            };
-        });
+        console.log(doneBtn.id, delBtn.id);
+
+        console.log(document.querySelectorAll("li").length);
 
     };
 
     inputTodo.value = '';
-    // errMsg1.style.display = "none";
+
     errMsg2.style.display = "none";
 }
+// to remove item form list
+itemList.addEventListener("click", (e) => {
+    if (e.target.id === "del-btn") {
+        confirm('ARE YOU SURE YOU WANT TO DELETE THIS TASK?');
+        e.target.parentElement.remove();
+        console.log(document.querySelectorAll("li").length);
+
+
+    } else if (e.target.id === "complete-btn") {
+        alert('Task completed');
+        e.target.parentElement.remove();
+        // displaying completed box and creating list in it
+        document.getElementById("completed").style.display = "block";
+        let compli = document.createElement("li");
+        // compli.innerHTML = e.target.parentElement.textNode;
+        document.getElementById("completed").appendChild(compli);
+        compli.id = "newli"
+
+        //creating a new delete btn
+        let delBtn = document.createElement('button');
+        delBtn.innerHTML = 'delete';
+        compli.appendChild(delBtn);
+        delBtn.id = "del-btn";
+        // adding event listener to the delete btn
+        document.getElementById("del-btn").addEventListener("click", (e) => {
+            confirm('ARE YOU SURE YOU WANT TO DELETE THIS TASK?');
+            e.target.parentElement.remove();
+
+
+        }, false);
+    }
+}, false);
